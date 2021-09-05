b9096286cfe3b3d3a7ef37ec38518874b1f7e5e6
Hisham T. Kamal-Al-Din <33468983+Hisham-TK@users.noreply.github.com>
2021-09-05 02:26:28 +0200

    edits

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
+# Build local
 
-# rm -rf ./platform/viewer/dist
-# yarn run build-docker
+# Get arguments
+while getopts v: flag; do
+  case "${flag}" in
+  v) version=${OPTARG} ;;
+  esac
+done
+echo "VERSION: $version"
+version=$(git rev-parse HEAD)
 
-# docker build -t gcr.io/vpacs-323411/ohif-viewer-test .
-# # docker build -t ohif-viewer-test .
-# # docker tag ohif-viewer-test gcr.io/vpacs-323411/ohif-viewer-test
+# Delete previous build & rebuild
+rm -rf ./platform/viewer/dist
+QUICK_BUILD=true APP_CONFIG=config/google.js yarn run build
 
-# docker push gcr.io/vpacs-323411/ohif-viewer-test
-
-
-docker build -t hello .
-docker run --rm --name=hello --inspect hello
+# Build and publish docker container
+docker build -t gcr.io/vpacs-323411/ohif-viewer-test:$version -f Dockerfile.local .
+docker push gcr.io/vpacs-323411/ohif-viewer-test:$version
